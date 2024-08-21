6df24f9e81be343bb134f618630a6c80632c3f1b
Eli <lagumbayeliyahu@gmail.com>
2024-08-21 11:34:31 +0800

    hi mike

---
+++ PracticeSets_02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeSets_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Write a program that prints all the even numbers from 1 to 20 using a for loop.
+             */
+
+        }
+    }
+}
